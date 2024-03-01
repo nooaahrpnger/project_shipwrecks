e33de275192f2ba5c9ce9db27976f2228ebd8f5b
KohJo792 <160712670+KohJo792@users.noreply.github.com>
2024-03-01 08:09:44 +0100

    Update delete.php
    New Version

--- includes/delete.php
+++ includes/delete.php
@@ -1,5 +1,5 @@
 <?php
  function deleteQuery($pTable, $pId){
     $deleteQuery = "DELETE  FROM $pTable WHERE id = '$pId'";
-    echo "Die Tabelle würder erfolgreich gelöscht!";
+    echo "Die Tabelle würder erfolgreich gelöscht!"; 
  }
