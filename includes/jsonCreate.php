d33c6790b2e351184260ab71ec43d4fc960db4a7
ThierryCecchini <rathalos1999@hotmail.de>
2024-03-01 08:31:27 +0100

    Insert (Updated)

--- includes/jsonCreate.php
+++ includes/jsonCreate.php
@@ -1,33 +1,29 @@
 <?php
-
 require_once "../db_credentials.php";
-$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PW, DB_NAME);
-mysqli_set_charset($dbc, "utf8");
 
-if(isset($_POST["createData"])) {
-    $wreckDate = mysqli_real_escape_string($dbc, $_POST["wreckDate"]);
-    $wreckName = mysqli_real_escape_string($dbc, $_POST["wreckName"]);
-    $country = mysqli_real_escape_string($dbc, $_POST["country"]);
-    $description = mysqli_real_escape_string($dbc, $_POST["description"]);
+function insertShipwreck($wreckDate, $wreckName, $country, $description) {
+    $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PW, DB_NAME);
+    mysqli_set_charset($dbc, "utf8");
 
     // Prepare statement
-    $stmt = mysqli_prepare($dbc, "INSERT INTO webap_shipwrecks (dtWreckageDate, dtShipName, dtCountryName, dtDescription) VALUES (?, ?, ?, ?)");
-    
+    $insertQuery = mysqli_prepare($dbc, "INSERT INTO webap_shipwrecks (dtWreckageDate, dtShipName, dtCountryName, dtDescription) VALUES (?, ?, ?, ?)");
+
     // Bind parameters
-    mysqli_stmt_bind_param($stmt, "ssss", $wreckDate, $wreckName, $country, $description);
+    mysqli_stmt_bind_param($insertQuery, "ssss", $wreckDate, $wreckName, $country, $description);
 
     // Execute statement
-    mysqli_stmt_execute($stmt);
+    mysqli_stmt_execute($insertQuery);
 
     // Check for errors
-    if(mysqli_stmt_error($stmt)) {
-        die(mysqli_stmt_error($stmt));
+    if(mysqli_stmt_error($insertQuery)) {
+        die(mysqli_stmt_error($insertQuery));
     }
 
     // Close statement
-    mysqli_stmt_close($stmt);
-}
+    mysqli_stmt_close($insertQuery);
 
-mysqli_close($dbc);
+    // Close database connection
+    mysqli_close($dbc);
+}
 
 ?>
