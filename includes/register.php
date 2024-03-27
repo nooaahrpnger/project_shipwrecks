ed6aa61a531b1fade3c73bc9ef9ca887aef3c0f2
XavierHoffmann <145866520+XavierHoffmann@users.noreply.github.com>
2024-03-27 09:48:54 +0100

    Register

--- includes/register.php
+++ includes/register.php
@@ -4,7 +4,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     $loginName = $_POST['SIGNIN_loginName'];
     $password = $_POST['SIGNIN_password'];
     $email = $_POST['SIGNIN_email'];
-    $accountType = "Admin"; // Annahme: Standard-Accounttyp ist "Admin"
+    $accountType = "Admin";
 
     require_once "../db_credentials.php";
     $conn = mysqli_connect(DB_HOST, DB_USER, DB_PW, DB_NAME);
@@ -14,7 +14,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
         die("Connection failed: " . mysqli_connect_error());
     }
 
-    // Die SQL-Abfrage sollte mit einem Semikolon abgeschlossen werden
     $sql = "INSERT INTO shipwrecks_Users (dtUsername, dtEmail, dtPassword, dtAccountType) VALUES ('$loginName', '$email', '$password', '$accountType')";
 
     if (mysqli_query($conn, $sql)) 
@@ -25,8 +24,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     {
         echo "Fehler beim Einfügen des Datensatzes: " . mysqli_error($conn);
     }
-
-    // Die Verbindung sollte nach dem Ausführen der SQL-Abfrage geschlossen werden
     mysqli_close($conn);
 }
 ?>
