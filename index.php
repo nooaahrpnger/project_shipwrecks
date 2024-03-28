33c0ea8b9d76bc1e878b0fcaf0ef3f4e81b0b5f6
ThierryCecchini <rathalos1999@hotmail.de>
2024-03-28 09:32:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -14,44 +14,6 @@
     <title>Shipwrecked</title>
 </head>
 <body style="margin: 0px;">
-<?php
-    /*
-    echo "<header >";
-            include_once "navigation.php";
-    echo "</header>";
-    */
-
-    /*---------------Navigation---------------*/
-
-    echo "<main>";
-    if(isset($_GET["modell"])){
-        $selectedCar = $_GET["modell"];
-    }
-
-    $page = "";
-    if (isset($_GET['page'])) {
-        $page = $_GET['page'];
-    }
-
-    if ($page == "XYZ") {
-        include_once "includes/XYZ.php";
-    }else{
-        include_once "includes/login.html";
-    }
-    echo "</main>";
-
-
-    /*----------------Footer--------------*/
-    echo "<br><div id='logo'><br>";
-        // echo "<img src='shipwreck logo' style='width: 7%; height: 7%; margin: auto;'><br><br>";
-    echo "</div><br>";
-    echo "<footer>";
-            //include_once "includes/footer.php";
-    echo "</footer>";
-
-?>
-
-
 
 </body>
 </html>
