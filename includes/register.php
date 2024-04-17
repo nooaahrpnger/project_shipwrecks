4368a9abe1d661886a363aa9854fce2596bf1558
XavierHoffmann <145866520+XavierHoffmann@users.noreply.github.com>
2024-04-17 09:17:56 +0200

    Register Update
    Quiz Result 0

--- includes/register.php
+++ includes/register.php
@@ -1,32 +1,53 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") 
-{
-    $loginName = $_POST['SIGNIN_loginName'];
-    $password = $_POST['SIGNIN_password'];
-    $email = $_POST['SIGNIN_email'];
-    $accountType = "user";
-
-    // Hash das Passwort
-    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-
-    require_once "../db_credentials.php";
-    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PW, DB_NAME);
-
-    if (!$conn) 
+    if ($_SERVER["REQUEST_METHOD"] == "POST") 
     {
-        die("Connection failed: " . mysqli_connect_error());
+        $loginName = $_POST['SIGNIN_loginName'];
+        $password = $_POST['SIGNIN_password'];
+        $email = $_POST['SIGNIN_email'];
+        $accountType = "user";
+        $quizResults = 0;
+
+        // Hash das Passwort
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        require_once "../db_credentials.php";
+        $conn = new mysqli(DB_HOST, DB_USER, DB_PW, DB_NAME);
+
+        // Überprüfe die Verbindung
+        if ($conn->connect_error) 
+        {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $stmt = $conn->prepare("INSERT INTO shipwrecks_Users (dtUsername, dtEmail, dtPassword, dtAccountType) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $loginName, $email, $hashedPassword, $accountType);
+
+        // Führe das Prepared Statement aus
+        if ($stmt->execute()) 
+        {
+            echo "Datensatz erfolgreich eingefügt. ";
+            $idUser = $stmt->insert_id;
+
+            $stmtQuiz = $conn->prepare("INSERT INTO shipwrecks_quiz (fiUser, dtQuizResults) VALUES (?, ?)");
+            $stmtQuiz->bind_param("ii", $idUser, $quizResults);
+
+            if ($stmtQuiz->execute()) 
+            {
+                echo "Benutzer erfolgreich zum Quiz hinzugefügt.";
+            } 
+            else 
+            {
+                echo "Fehler beim Hinzufügen des Benutzers zum Quiz: " . $stmtQuiz->error;
+            }
+
+            $stmtQuiz->close();
+        } 
+        else 
+        {
+            echo "Fehler beim Einfügen des Datensatzes: " . $stmt->error;
+        }
+        
+        $stmt->close();
+        $conn->close();
     }
-
-    $sql = "INSERT INTO shipwrecks_Users (dtUsername, dtEmail, dtPassword, dtAccountType) VALUES ('$loginName', '$email', '$hashedPassword', '$accountType')";
-
-    if (mysqli_query($conn, $sql)) 
-    {
-        echo "Datensatz erfolgreich eingefügt.";
-    } 
-    else 
-    {
-        echo "Fehler beim Einfügen des Datensatzes: " . mysqli_error($conn);
-    }
-    mysqli_close($conn);
-}
 ?>
