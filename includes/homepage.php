85e32111574b7cbb8d14b84c7198d8f33d0fd406
KohJo792 <160712670+KohJo792@users.noreply.github.com>
2024-04-19 08:39:35 +0200

    Home Text
    Change

--- includes/homepage.php
+++ includes/homepage.php
@@ -7,9 +7,9 @@
         Our platform showcases vessels, such as the "Gannant," 
         along with their unfortunate demise dates, the countries where they sank,
         and brief descriptions of the disasters.
-        The "Gannant," for instance, met its fate on [date]
-        off the coast of [country]. This tragic event is remembered for
-        [brief description of the disaster]. Through our platform,
+        The "Gannant," for instance, met its fate on date
+        off the coast of country. This tragic event is remembered for
+        brief description of the disaster. Through our platform,
         we aim to honor the memory of these lost ships and the stories they carry, 
         serving as a testament to the power and unpredictability of the sea.
         Explore our website to delve into the fascinating world of maritime 
