28f9d3b333a4afc19441431f12c237bbdd8024b1
XavierHoffmann <145866520+XavierHoffmann@users.noreply.github.com>
2024-04-19 08:41:42 +0200

    ShopVerlinkung

--- includes/header.php
+++ includes/header.php
@@ -7,7 +7,7 @@
             <li><a href="index.php?page=includes/homepage.php">Home</a></li>
             <li><a href="index.php?page=includes/quiz.html">Quiz</a></li>
             <li><a href="index.php?page=includes/searchShip.html">Search</a></li>
-           
+            <li><a href="index.php?page=includes/Shop/shop.html">Shop</a></li>
         </ul>
     </nav>
 </header>
