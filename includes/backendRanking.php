a1396c3bfae4db04dcbecd3869440ef51ecf01a3
LucNtw <101431449+LucNtw@users.noreply.github.com>
2024-05-07 19:20:24 +0200

    kommentiert

--- includes/backendRanking.php
+++ includes/backendRanking.php
@@ -1,25 +1,35 @@
+
 <?php
 require_once "db_credentials.php";
-
 $conn = new mysqli(DB_HOST, DB_USER, DB_PW, DB_NAME);
+
+// Verbindung überprüfen
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Verbindung fehlgeschlagen: " . $conn->connect_error);
 }
 
+// SQL-Abfrage für die Quizergebnisse
 $sql = "SELECT dtUsername, dtQuizResults FROM shipwrecks_quiz JOIN shipwrecks_Users ON fiUser = idUser ORDER BY dtQuizResults DESC";
+
+// SQL-Abfrage ausführen
 $result = $conn->query($sql);
 
-$data = array(); 
+// Array für die Quizergebnisse 
+$data = array();
 
+// Quizergebnisse aus der Datenbank abrufen und in das Array hinzufügen
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        $data[] = $row; 
+        $data[] = $row;
     }
 }
 
 
 header('Content-Type: application/json');
+
+// Quizergebnisse als JSON ausgeben
 echo json_encode($data);
 
+// Verbindung schließen
 $conn->close();
 ?>
