940ca959f9be7fc30ca3377ab89b4ec04536f2aa
XavierHoffmann <145866520+XavierHoffmann@users.noreply.github.com>
2024-05-07 16:23:31 +0200

    header icons

--- includes/header.php
+++ includes/header.php
@@ -1,19 +1,20 @@
 <div class="logo">
     <h1>Shipwrecks</h1>
 </div>
+<script src="https://kit.fontawesome.com/95ad3152a3.js" crossorigin="anonymous"></script>
 <nav>
     <ul>
         <li><a href="index.php?page=includes/homepage.php">Home</a></li>
         <li><a href="index.php?page=includes/quiz.html">Quiz</a></li>
         <li><a href="index.php?page=includes/searchShip.html">Search</a></li>
-        <li><a href="index.php?page=includes/Shop/shop.html">Shop</a></li>
+        <li><a href="index.php?page=includes/Shop/shop.html"><i class="fa-solid fa-shop"></i></a></li>
         
         <?php
         if(isset($_SESSION["LOGIN_user"]))
         {
             ?>
-            <li><a href="index.php?page=includes/Shop/checkout.php">Cart</a></li>
-            <li><a href="index.php?page=includes/logout.php">Logout</a></li>
+            <li><a href="index.php?page=includes/Shop/checkout.php"><i class="fa-solid fa-cart-shopping"></i></a></li>
+            <li><a href="index.php?page=includes/logout.php"><i class="fa-solid fa-right-from-bracket"></i></a></li>
             <li> <?php $_SESSION["LOGIN_user"] ?> </li>
             <?php
         }
